8c4eb8f3bd90e7427a1004768109182f098e0d17
Květoslav Bastard <rainbowflippy@gmail.com>
2020-06-16 14:21:36 +0200

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -1,68 +1,5 @@
 #include QMK_KEYBOARD_H
 
-/*Keycodes I guess*/
-//Cesky znaky
-#define CZ_PLUS KC_1
-#define CZ_ES KC_2
-#define CZ_SS KC_3
-#define CZ_CC KC_4
-#define CZ_RR KC_5
-#define CZ_ZZ KC_6
-#define CZ_YY KC_7
-#define CZ_AA KC_8
-#define CZ_II KC_9
-#define CZ_EE KC_0
-
-//FN
-#define CZ_LSFT MO(1)
-#define CZ_NUMB MO(3)
-#define CZ_CZ   MO(2)
-#define CZ_FN   LT(9, KC_TAB)
-#define CZ_SYS  MO(9)
-
-
-//Default
-#define CZ_MINS KC_SLSH
-#define CZ_CARK KC_EQL
-#define CZ_LBRC RALT(KC_F)
-#define CZ_RBRC RALT(KC_G)
-#define CZ_SCLN KC_GRV
-#define CZ_APOS LSFT(KC_BSLS)
-#define CZ_BSLS RALT(KC_Q)
-#define CZ_SLSH LSFT(KC_LBRC)
-
-//Shift US
-#define CZ_TILD RALT(KC_1)
-#define CZ_EXLM KC_DQUO
-#define CZ_AT   RALT(KC_V)
-#define CZ_HASH RALT(KC_X)
-#define CZ_DLR  RALT(KC_SCLN)
-#define CZ_PERC KC_UNDS
-#define CZ_CIRC RALT(KC_6) //Upravit delay
-#define CZ_AMPR RALT(KC_C)
-#define CZ_ASTR RALT(KC_SLSH)
-#define CZ_LPRN LSFT(KC_RBRC)
-#define CZ_RPRN KC_RBRC
-#define CZ_UNDS LSFT(KC_SLSH)
-#define CZ_HACK LSFT(KC_EQL)
-
-#define CZ_LCBR RALT(KC_B)
-#define CZ_RCBR RALT(KC_N)
-#define CZ_COLN LSFT(KC_DOT)
-#define CZ_DQT  LSFT(KC_SCLN)
-#define CZ_PIPE RALT(KC_W)
-#define CZ_LABK RALT(KC_COMM)
-#define CZ_RABK RALT(KC_DOT)
-#define CZ_QUES LSFT(KC_COMM)
-
-
-//number layer
-#define CZ_EQLS KC_MINS
-#define CZ_USHC KC_LBRC
-#define CZ_USKR KC_SCLN
-
-
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//Default 
@@ -75,7 +12,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	
-	
 	//Shift + US layer
 	//Shift
 	//Layer 1
@@ -87,7 +23,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,    LSFT(KC_Z), LSFT(KC_X), LSFT(KC_C), LSFT(KC_V), LSFT(KC_B), LSFT(KC_N), LSFT(KC_M), CZ_LABK,    CZ_RABK,    CZ_QUES,                KC_RSFT,      _______,
 		KC_TRNS, 	KC_TRNS,	KC_TRNS,											KC_TRNS,												KC_TRNS,    KC_TRNS, 	  KC_TRNS
 	),
-	
 	
 	
 
